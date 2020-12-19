213ccee1ac669f9c93fc83452b974a940b51f4e3
Thomas Bocklitz <72965520+thomasbocklitz82@users.noreply.github.com>
2020-12-19 19:28:02 +0100

    changes ...

--- Aus.R
+++ Aus.R
@@ -1,5 +1,23 @@
-plot(1:10)
+require("scholar")
+Typ<-"FUiFDqUAAAAJ&hl"
+Profile<-get_profile(Typ)
+Publications<-get_publications(Typ)
+Impact<-get_impactfactor(journals=unique(Publications$journal)
+                         , max.distance = 0.05)
 
-lines(1:10+0.1)
+hist(Publications$year)
 
-points(1:10,pch=4)
+Pubid<-as.character(Publications$pubid[1])
+Cid<-as.character(Publications$cid[1])
+
+TEMP<-get_article_cite_history(id=Typ, article=Pubid)
+
+get_num_articles(Typ)
+get_num_distinct_journals(Typ)
+get_oldest_article(Typ)
+Cit.Hist<-get_citation_history(id=Typ)
+plot(Cit.Hist$cites~Cit.Hist$year)
+
+TMP<-get_complete_authors(id=Typ, pubid=Cid)
+
+
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-Citation_Vis
+## Citation_Vis Project to extract Infos from Google scholar
 
-## Citations
